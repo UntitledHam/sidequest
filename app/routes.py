14d2c1643da619b13cc06e7af94e03dd603ba407
untitledham <abemanfra@gmail.com>
2025-04-08 21:00:53 -0400

    Mocked up buildings gui

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from flask import render_template
+
 from app import app, db
 
 
 @app.route("/")
 def index():
-    return render_template("home.html", title="Home")
+    buildings = [
+        {
+            "name": "Gup",
+            "description": "A small cat that generates points by purring."
+        },
+        {
+            "name": "Skinnerbox",
+            "description": "A box with a small rat that generates points by clicking a button.",
+        },
+    ]
+    return render_template("home.html", title="Home", buildings=buildings)
