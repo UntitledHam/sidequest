85fecc68079b955b92a72b802fcd71459c369bdb
untitledham <abemanfra@gmail.com>
2025-04-30 23:18:07 -0400

    Added my service file to the git

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# Change working directory to the path of sidequest.
-cd "$(dirname "$0")"; 
 
 # Source the .venv
-source ".venv/bin/activate";
+source ".venv/bin/activate"
 
 # Run the program 
-waitress-serve --host 127.0.0.1 app:app;
+exec waitress-serve --host 127.0.0.1 app:app
